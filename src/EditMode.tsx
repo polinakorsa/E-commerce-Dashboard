139354cf301ff3fa6224c2036198999674b941dd
Paulina Korsa <paulinakorsa@Paulinas-MacBook-Air.local>
2025-12-23 22:42:13 +0200

    Commit dev progress

--- src/EditMode.tsx
+++ src/EditMode.tsx
@@ -3,28 +3,6 @@ export default function EditMode() {
 
     return (
         <>
-            <input
-                value={editedUser.firstName || ''}
-                className='px-3 py-2 border rounded-md w-40'
-            />
-            <input
-                value={editedUser.lastName || ''}
-                className='px-3 py-2 border rounded-md w-40'
-            />
-            <input
-                value={editedUser.age || ''}
-                type='number'
-                className='px-3 py-2 border rounded-md w-24'
-            />
-            <input
-                value={editedUser.email || ''}
-                className='px-3 py-2 border rounded-md w-64'
-            />
-            <input
-                value={editedUser.password || ''}
-                type='password'
-                className='px-3 py-2 border rounded-md w-40'
-            />
         </>
     )
 }
